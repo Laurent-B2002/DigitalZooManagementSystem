7934617f810d4d3374fe6893d13a9edfc836c38f
LegendaryLoona <139046578+LegendaryLoona@users.noreply.github.com>
2025-02-27 12:04:31 +0100

    Added view all habitats and deletion functions

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import AnimalList from "./components/AnimalList";
+import { AnimalList, HabitatList } from "./components/AnimalList";
 import AddAnimalForm from "./components/AddAnimal";
+import { DeleteAnimalForm, DeleteHabitatForm } from "./components/Deletion";
 
 function App() {
     return (
         <div>
             <h1>Welcome to the Animal App!</h1>
             <AddAnimalForm />
+            <DeleteAnimalForm />
+            <DeleteHabitatForm />
             <AnimalList />
+            <HabitatList />
         </div>
     );
 }
